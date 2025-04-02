0789b0e81e05736540abb6fb4510f51e53274f90
Adarsh Singh <adarsh0727@gmail.com>
2025-04-02 11:21:42 +0530

    added utility function

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import { verifyToken } from "../utils/jwt.utils.js";
 
 export const protect = async (req, res, next) => {
     let token;
@@ -8,7 +8,7 @@ export const protect = async (req, res, next) => {
         try {
             token = req.headers.authorization.split(" ")[1];
 
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const decoded = verifyToken(token); 
 
             req.user = await User.findById(decoded.id).select("-password");
 
